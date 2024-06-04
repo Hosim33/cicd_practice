5a9449bc7d5be74f99f65e3850ec531690babe16
hosim33 <ghtla0330@naver.com>
2024-06-04 16:56:22 +0900

    add cloud path

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/cloud')
+def hello_cloud():
+    return 'AWS, Azure, GCP!'
+
 if __name__ == '__main__':
     app.run()
